fdc41888d8a4663428b670bd04e664217beb7ade
Anthony Langsworth <anthony.langsworth@gmail.com>
2025-01-04 17:42:43 +1100

    Split body loading into separate project

--- Colonisation.Bodies/Program.cs
+++ Colonisation.Bodies/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello world!");
+﻿using Microsoft.Extensions.Configuration;
+using CsvHelper;
+using System.Globalization;
+using Colonisation.Common;
+
+IConfigurationRoot configurationRoot = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+
+using HttpClient httpClient = new HttpClient();
+
+using StreamReader inputFile = new(configurationRoot["colonisationTargetsFileName"] ?? "");
+using CsvReader csvReader = new(inputFile, CultureInfo.InvariantCulture);
+csvReader.Context.RegisterClassMap<StarSystemOutputClassMap>();
+foreach(StarSystemOutput starSystem in csvReader.EnumerateRecords(new StarSystemOutput()))
+{
+    string details = await httpClient.GetStringAsync($"https://www.edsm.net/api-system-v1/bodies?systemName={starSystem.name}");
+    Console.Out.WriteLine(details);
+}
--- Colonisation.Common/POCOs.cs
+++ Colonisation.Common/POCOs.cs
@@ -1,4 +1,6 @@
-﻿namespace Colonisation.Common;
+﻿using CsvHelper.Configuration;
+
+namespace Colonisation.Common;
 
 public record Coords
 {
@@ -28,4 +30,14 @@ public record StarSystemOutput
     public string name = "";
     public string nearestEdaSystemName = "";
     public double distance = 0.0;
+}
+
+public class StarSystemOutputClassMap : ClassMap<StarSystemOutput>
+{
+    public StarSystemOutputClassMap()
+    {
+        Map(ssi => ssi.name).Name("Name").Index(0);
+        Map(ssi => ssi.nearestEdaSystemName).Name("Nearest EDA System").Index(1);
+        Map(ssi => ssi.distance).Name("Distance").Index(2);
+    }
 }
