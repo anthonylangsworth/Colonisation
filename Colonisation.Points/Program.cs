6ba7cd35822e233071da2a54dfc6b66953d2d253
Anthony Langsworth <anthony.langsworth@gmail.com>
2025-01-05 22:43:02 +1100

    Added sample systemBodies.json. Started Points.

---
+++ Colonisation.Points/Program.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using CsvHelper;
+using Microsoft.Extensions.Configuration;
+using Colonisation.Common;
+using Microsoft.Extensions.Logging;
+
+// See README.md for details. Error handling is intentionally minimal to improve clarity and speed development.
+
+// Sample of systemsWithCoordinates.json for reference:
+// [ {"id":18517,"id64":9468121064873,"name":"Kunti","coords":{"x":88.65625,"y":-59.625,"z":-4.0625},"date":"2017-02-24 09:42:54"} ]
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appSettings.json").Build();
+using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+ILogger logger = loggerFactory.CreateLogger("Default");
